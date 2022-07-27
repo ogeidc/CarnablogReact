dc83f4b5aecdd13456bb64a9793f2367ff05ba1d
Diego Candido da Silva <ogeid.csilva@gmail.com>
2022-07-27 11:16:14 -0300

    Primeiros passos do React no Carnablog

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo"> Carnablog</h1>
+            <p className="center"> Sua fonte especial de desfiles memoráveis</p>
+            <img src="https://i.imgur.com/cnkFp9d.jpeg" alt="Portela em 2015" className="img"></img>
+        </>
+    );
+}
+export default Home;
